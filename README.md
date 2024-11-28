6302c634532022574e9d3fc9e03611d26fd14ae0
blake98x <blakeet@umich.edu>
2024-11-27 23:24:53 -0500

    Updated README and added regs/guidelines support

--- README.md
+++ README.md
@@ -1,51 +1,71 @@
+
 # WCA LangChain Model
+
+  
 
 ## Context:
 
-The WCA is a global and volunteer-driven organization that “governs competitions for mechanical puzzles that are operated by twisting groups of pieces, commonly known as 'twisty puzzles’” [[1](https://www.worldcubeassociation.org/about)]. As the organization has evolved through expanding its global footprint and spreading the unique passion of solving Rubik’s Cubes, it has needed to adapt to the intricacies of such dramatic growth. With its volunteer base stemming from such a variety of backgrounds, there has been need to establish regulations, policies, and guidelines. 
+  
 
-While these documents help to ensure that the variety of Delegates abide by and enforce the same standards across the globe, the sheer quantity/depth of them along with the language barriers that exist within the organization make understanding the depth of these documents a remarkably difficult task.
+The WCA is a global and volunteer-driven organization that “governs competitions for mechanical puzzles that are operated by twisting groups of pieces, commonly known as 'twisty puzzles’” [[1](https://www.worldcubeassociation.org/about)]. As the organization has evolved through expanding its global footprint and spreading the unique passion of solving Rubik’s Cubes, it has needed to adapt to the intricacies of such dramatic growth. With its volunteer base stemming from such a variety of backgrounds, there has been need to establish regulations, policies, and guidelines.
 
-The tool developed here seeks to allow for quick responses primarily for the following documents:
+While these documents help to ensure that the variety of Delegates abide by and enforce the same standards across the globe, the sheer quantity/depth of them along with the language barriers that exist within the organization make understanding the depth of these documents a remarkably difficult task. There also exists a wealth of Regulations and Guidelines with which all competitors are expected to be familiar. 
 
-1. WCA Regulations
-2. WCA Guidelines
-3. WCA Bylaws
-4. WCA Motions
-5. WCA Code of Conduct
-6. WCA Vision and Strategy
-7. WCA Minutes
-8. WCA Financial Reports
-9. WCA Competition Requirements Policy
-10. WCA Dues System
-11. WCA Equipment Funding Policy
-12. WCA Logo Usage Policy
-13. WCA Regional Organization Support Policy
-14. WCA Scramble Accountability Policy
-15. WCA Travel Reimbursement Policy
+  
+
+The tool developed here seeks to allow for quick responses to address two fundamental use cases:
+
+ 1. Questions about the WCA Regulations and Guidelines
+ 2. Questions about any other WCA document including the following:
+    - WCA Bylaws
+    - WCA Motions
+    - WCA Code of Conduct
+    - WCA Vision and Strategy
+    - WCA Minutes
+    - WCA Financial Reports
+    - WCA Competition Requirements Policy
+    - WCA Dues System
+    - WCA Equipment Funding Policy
+    - WCA Logo Usage Policy
+    - WCA Regional Organization Support Policy
+    - WCA Scramble Accountability Policy
+    - WCA Travel Reimbursement Policy
+  
 
 ## How does it work?
 
-## Installation:
+This project leverages the surge of popularity and utility in LLMs as a means of developing a chatbot for a hyper-specific use-case. We lean heavily on the use of LangChain, which is a "framework for developing applications powered by large language models (LLMs)" within Python [\[3\]](https://python.langchain.com/docs/introduction/). In particular, we use the RAG pipeline -- which begins by ingesting and indexing the documents we send into it, and a retriever "takes the user query at run time, retrieves the relevant data from the index, [and] then passes that to the model [\[4\]](https://python.langchain.com/docs/tutorials/rag/)."
 
 ## Usage:
 
-```
-streamlit run app.py
+  
+
 ```
 
-## Reflection:
+streamlit run app.py
+
+```
+
+
+  
 
 ## Future Steps:
 
-- [ ]  Adopt this approach for other sports with niche guidelines and policies (e.g., Chess).
+  
 
-## Contributing:
+- [ ] Adopt this approach for other sports with niche guidelines and policies (e.g., Chess).
+
+- [ ] Continue to refine this approach and investigate methods to improve...
+	- [ ] Accuracy
+	- [ ] Loading time
+	- [ ] User Interface
+
 
 ## Resources:
 
+  
+
 [1] https://www.worldcubeassociation.org/about
-
 [2] https://www.youtube.com/watch?v=wUAUdEw5oxM
-
-[3] https://python.langchain.com/docs/use_cases/question_answering/
+[3] https://python.langchain.com/docs/introduction/
+[4] https://python.langchain.com/docs/tutorials/rag/
--- app.py
+++ app.py
@@ -11,111 +11,108 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
 from langchain.chat_models import ChatOpenAI
+
 #%%
 
-def import_documents(url, downloaded_documents):
-    # Create the directory to store the documents found
-    if not os.path.exists('Documents'):
-        os.makedirs('Documents')
+def import_documents(url, downloaded_documents, folder_name):
+    # Create the directory to store the documents
+    folder_path = os.path.join('Documents', folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
     # Send a GET request to the URL
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     pdf_links = soup.find_all('a', href=lambda href: (href and href.endswith('.pdf')))
 
-    # Download each PDF file
+    # Download each unique PDF file
     for link in pdf_links:
         pdf_url = link['href']
         pdf_filename = pdf_url.split('/')[-1]
         if pdf_filename not in downloaded_documents:
-            pdf_path = os.path.join('Documents', pdf_filename)
+            pdf_path = os.path.join(folder_path, pdf_filename)
             pdf_url = urllib.parse.urljoin(url, pdf_url.replace(' ', '%20'))
             print("Downloading:", pdf_filename)
             with open(pdf_path, 'wb') as f:
                 f.write(requests.get(pdf_url).content)
 
 def create_page():
-    st.set_page_config(page_title = "Ask a Question about a WCA Document!")
+    st.set_page_config(page_title="Ask a Question about a WCA Document!")
     st.header("What do you want to know about the WCA?")
 
-def read_pdfs(downloaded_documents):
-
-    for index, doc in enumerate(downloaded_documents):
-        pdf_reader = PdfReader('Documents/' + doc)
-        text = ''
+def get_pdf_text(folder_name):
+    text = ''
+    folder_path = os.path.join('Documents', folder_name)
+    for doc in os.listdir(folder_path):
+        pdf_reader = PdfReader(os.path.join(folder_path, doc))
         for page in pdf_reader.pages:
             text += page.extract_text()
-         
-        # Split into chunks
-        text_splitter = CharacterTextSplitter(
-            separator = "\n",
-            chunk_size = 1000,
-            chunk_overlap = 200,
-            length_function = len
-        )
+    return text
 
-        chunks = text_splitter.split_text(text)
+def get_text_chunks(text):
+    # Split into chunks
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-        embeddings = OpenAIEmbeddings()
+def get_vectorstore(chunks):
+    embeddings = OpenAIEmbeddings()
+    return FAISS.from_texts(chunks, embeddings)
 
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+def ask_question(knowledge_base, context_name):
+    user_question = st.text_input(f"Ask a question about {context_name}!")
+    if user_question:
+        docs = knowledge_base.similarity_search(user_question)
 
-        user_question = st.text_input("Ask a question about the WCA!")
+        llm = ChatOpenAI()
+        chain = load_qa_chain(llm, chain_type='stuff')
 
-        if user_question:
-            docs = knowledge_base.similarity_search(user_question)
-
-            llm = ChatOpenAI(model="gpt-4-turbo-preview")
-
-            # Load the QA chain
-            chain = load_qa_chain(llm, chain_type='stuff')
-
-            # Perform question answering
-            response = chain.run(input_documents=docs, question=user_question)
-
-            st.write(response)
+        # Perform question answering
+        response = chain.run(input_documents=docs, question=user_question)
+        st.write(response)
 
 def main():
-
     load_dotenv()
 
     # Set URLs of WCA's documents page and Regulations/Guidelines
-    urls = [
-        'https://www.worldcubeassociation.org/documents',
-        'https://www.worldcubeassociation.org/regulations/',
-        'https://www.worldcubeassociation.org/regulations/guidelines.html'
-    ]
+    urls = {
+        "Documents": 'https://www.worldcubeassociation.org/documents',
+        "Regulations and Guidelines": 'https://www.worldcubeassociation.org/regulations/full/'
+    }
 
-    downloaded_documents = os.listdir('Documents')
+    # Create necessary folders
+    downloaded_documents = {
+        context: os.listdir(os.path.join('Documents', context)) if os.path.exists(os.path.join('Documents', context)) else []
+        for context in urls.keys()
+    }
 
-    for url in urls:
-        import_documents(url, downloaded_documents)
+    for context, url in urls.items():
+        import_documents(url, downloaded_documents[context], context)
 
     print('\n 🎉 All Documents have been downloaded! 🎉')
 
     create_page()
 
-    read_pdfs(downloaded_documents[0:1])
+    # Add toggle for selecting the context
+    context_name = st.selectbox(
+        "Choose a context for your question:",
+        list(urls.keys())
+    )
 
+    # Load the text and create a vectorstore
+    raw_text = get_pdf_text(context_name)
+    text_chunks = get_text_chunks(raw_text)
+    knowledge_base = get_vectorstore(text_chunks)
 
-
-
+    ask_question(knowledge_base, context_name)
 
 if __name__ == '__main__':
     main()
 
-
-
 # %%
-
-'''
-TODO:
-
-- Work out loading speed
-- Test on wider span of documents
-
-
-'''
